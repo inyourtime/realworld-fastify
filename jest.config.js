2e08630e60031b16c5a416281cc3c65f8986814f
sahachai.t <sahachai@edvisory.co.th>
2024-01-11 17:11:42 +0700

    remove jest js file

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jestConfig = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-};
-exports.default = jestConfig;

60fc5224c2116d7f66f5593944c3b77ac774b396
sahachai.t <sahachai@edvisory.co.th>
2024-01-15 21:43:39 +0700

    asd

--- src/plugins/__tests__/authen-fp.spec.ts
+++ src/plugins/__tests__/authen-fp.spec.ts
@@ -4,10 +4,18 @@ import plugin, {
   ERR_TOKEN_EXPIRED,
   ERR_TOKEN_INVALID,
 } from '../authenticate';
-import { generateAccessToken } from '../../utils/token';
+import { verifyToken } from '../../utils/token';
 import { STATUS_CODE } from '../../internal/fastify/responseHandler/statusCode';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
-const testCase = [
+jest.mock('../../utils/token.ts', () => ({
+  verifyToken: jest.fn((token: string, secret: string) => ({
+    error: null,
+    result: 'success',
+  })),
+}));
+
+const cases = [
   {
     auth: false,
     headerToken: '',
@@ -28,50 +36,140 @@ const testCase = [
   },
   {
     auth: true,
-    headerToken: 'Bearer asdf',
-    expectedCode: ERR_TOKEN_INVALID.statusCode,
-    expectedErrMessage: ERR_TOKEN_INVALID.message,
-  },
-  {
-    auth: true,
-    headerToken: `Bearer ${generateAccessToken({}, undefined, {
-      expiresIn: '1',
-    })}`,
-    expectedCode: ERR_TOKEN_EXPIRED.statusCode,
-    expectedErrMessage: ERR_TOKEN_EXPIRED.message,
-  },
-  {
-    auth: true,
-    headerToken: `Bearer ${generateAccessToken({}, undefined, {
-      expiresIn: '1d',
-    })}`,
+    headerToken: `Bearer asdf`,
     expectedCode: STATUS_CODE.OK,
     expectedErrMessage: '',
   },
 ];
 
 describe('Test authentication plugin', () => {
-  it.each(testCase)('test 1 2 3 4', async (c) => {
-    const fastify = Fastify();
-    fastify.register(plugin);
-    fastify.route({
-      method: 'GET',
-      url: '/test',
-      config: {
-        auth: c.auth,
-      },
-      handler: (req, res) => ({ message: 'done' }),
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('general case', () => {
+    it.each(cases)('test 1 2 3 4', async (c) => {
+      const fastify = Fastify();
+      fastify.register(plugin);
+      fastify.route({
+        method: 'GET',
+        url: '/test',
+        config: {
+          auth: c.auth,
+        },
+        handler: (req, res) => ({ message: 'done' }),
+      });
+
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/test',
+        headers: { authorization: c.headerToken },
+      });
+
+      expect(res.statusCode).toBe(c.expectedCode);
+      if (c.expectedCode !== STATUS_CODE.OK) {
+        expect(JSON.parse(res.body).message).toEqual(c.expectedErrMessage);
+      }
+    });
+  });
+
+  describe('error case', () => {
+    it('should invalid token', async () => {
+      (verifyToken as jest.Mock).mockReturnValueOnce({
+        error: new JsonWebTokenError('invalid'),
+      });
+
+      const fastify = Fastify();
+      fastify.register(plugin);
+      fastify.route({
+        method: 'GET',
+        url: '/test',
+        config: {
+          auth: true,
+        },
+        handler: (req, res) => ({ message: 'done' }),
+      });
+
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/test',
+        headers: { authorization: 'Bearer asdf' },
+      });
+
+      expect(res.statusCode).toBe(ERR_TOKEN_INVALID.statusCode);
+      expect(JSON.parse(res.body).message).toEqual(ERR_TOKEN_INVALID.message);
     });
 
-    const res = await fastify.inject({
-      method: 'GET',
-      url: '/test',
-      headers: { authorization: c.headerToken },
+    it('should expired token', async () => {
+      (verifyToken as jest.Mock).mockReturnValueOnce({
+        error: new TokenExpiredError('expired', new Date()),
+      });
+
+      const fastify = Fastify();
+      fastify.register(plugin);
+      fastify.route({
+        method: 'GET',
+        url: '/test',
+        config: {
+          auth: true,
+        },
+        handler: (req, res) => ({ message: 'done' }),
+      });
+
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/test',
+        headers: { authorization: 'Bearer asdf' },
+      });
+
+      expect(res.statusCode).toBe(ERR_TOKEN_EXPIRED.statusCode);
+      expect(JSON.parse(res.body).message).toEqual(ERR_TOKEN_EXPIRED.message);
     });
 
-    expect(res.statusCode).toBe(c.expectedCode);
-    if (c.expectedCode !== STATUS_CODE.OK) {
-      expect(JSON.parse(res.body).message).toEqual(c.expectedErrMessage);
-    }
+    it('unexpected error', async () => {
+      (verifyToken as jest.Mock).mockReturnValueOnce({
+        error: new Error('error'),
+      });
+
+      const fastify = Fastify();
+      fastify.register(plugin);
+      fastify.route({
+        method: 'GET',
+        url: '/test',
+        config: {
+          auth: true,
+        },
+        handler: (req, res) => ({ message: 'done' }),
+      });
+
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/test',
+        headers: { authorization: 'Bearer asdf' },
+      });
+
+      expect(res.statusCode).toBe(STATUS_CODE.INTERNAL_SERVER_ERROR);
+    });
+
+    it('not found route', async () => {
+      const fastify = Fastify();
+      fastify.register(plugin);
+      fastify.route({
+        method: 'GET',
+        url: '/test',
+        config: {
+          auth: true,
+        },
+        handler: (req, res) => ({ message: 'done' }),
+      });
+
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/test1',
+        headers: { authorization: 'Bearer asdf' },
+      });
+
+      expect(res.statusCode).toBe(STATUS_CODE.NOT_FOUND);
+    });
   });
 });
---
+++ src/utils/__tests__/bcrypt.spec.ts
@@ -0,0 +1,79 @@
+import { hashPassword, checkPassword } from '../bcrypt';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt', () => ({
+  genSaltSync: jest.fn(() => 'mockedSalt'),
+  hash: jest.fn((plain: string, salt: string) =>
+    Promise.resolve(`hashed:${plain}:${salt}`),
+  ),
+  compare: jest.fn((plain: string, hash: string) =>
+    Promise.resolve(hash.startsWith(`hashed:${plain}:`)),
+  ),
+}));
+
+describe('Auth functions', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('hash password', () => {
+    it('should hash the password correctly', async () => {
+      const hashedPassword = await hashPassword('password');
+      expect(hashedPassword).toBe('hashed:password:mockedSalt');
+      expect(bcrypt.genSaltSync).toHaveBeenCalledWith(10);
+      expect(bcrypt.hash).toHaveBeenCalledWith('password', 'mockedSalt');
+    });
+
+    it('should reject with an error if hashing fails', async () => {
+      // Simulate a rejection from bcrypt.hash
+      (bcrypt.hash as jest.Mock).mockRejectedValueOnce(
+        new Error('Hashing failed'),
+      );
+
+      await expect(hashPassword('password')).rejects.toThrow('Hashing failed');
+      expect(bcrypt.genSaltSync).toHaveBeenCalledWith(10);
+      expect(bcrypt.hash).toHaveBeenCalledWith('password', 'mockedSalt');
+    });
+  });
+
+  describe('checkPassword', () => {
+    it('should return true for correct password', async () => {
+      const result = await checkPassword(
+        'hashed:password:mockedSalt',
+        'password',
+      );
+      expect(result).toBe(true);
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        'password',
+        'hashed:password:mockedSalt',
+      );
+    });
+
+    it('should return false for incorrect password', async () => {
+      const result = await checkPassword(
+        'hashed:wrongpassword:mockedSalt',
+        'password',
+      );
+      expect(result).toBe(false);
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        'password',
+        'hashed:wrongpassword:mockedSalt',
+      );
+    });
+
+    it('should reject with an error if bcrypt.compare fails', async () => {
+      // Simulate a rejection from bcrypt.compare
+      (bcrypt.compare as jest.Mock).mockRejectedValueOnce(
+        new Error('Comparison failed'),
+      );
+
+      await expect(
+        checkPassword('hashed:password:mockedSalt', 'password'),
+      ).rejects.toThrow('Comparison failed');
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        'password',
+        'hashed:password:mockedSalt',
+      );
+    });
+  });
+});
